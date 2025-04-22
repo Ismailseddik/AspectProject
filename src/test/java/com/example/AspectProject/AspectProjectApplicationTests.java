3f28b3f057f311d1a5ab68ab30879f0ae6e55b13
Ismailseddik <ismailahmed200101@gmail.com>
2025-04-22 20:58:24 +0200

    Initial commit

---
+++ src/test/java/com/example/AspectProject/AspectProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AspectProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AspectProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
